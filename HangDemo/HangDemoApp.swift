602f3f429d12145bd198514029258e2deedee615
hsinlei <hsinlei.w@gmail.com>
2024-04-20 10:30:44 -1000

    Initial Commit

---
+++ HangDemo/HangDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangDemoApp.swift
+//  HangDemo
+//
+//  Created by Chris Liu on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct HangDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
